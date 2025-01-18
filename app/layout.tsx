52b7256f1103b2f83e771bf0cf97d70b4a5d3638
Mwungere <mwungereelite56@gmail.com>
2025-01-18 23:53:51 +0200

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { SheetProvider } from "@/providers/sheet-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Finance Sass",
+  title: "Capital Manager",
   description: "This is your Financial Service",
 };
 
